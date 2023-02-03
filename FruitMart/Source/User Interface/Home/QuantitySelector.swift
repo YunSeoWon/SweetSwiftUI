4ada68d8649bf28adae76a47d4a15c0474a3915f
YunSeoWon <floidea@naver.com>
2023-02-04 00:35:17 +0900

    [Ch5] 12. 햅틱 피드백 적용하기

--- FruitMart/Source/User Interface/Home/QuantitySelector.swift
+++ FruitMart/Source/User Interface/Home/QuantitySelector.swift
@@ -11,6 +11,13 @@ import SwiftUI
 struct QuantitySelector: View {
     @Binding var quantity: Int
     
+    /**
+     *  햅틱 피드백 적용
+     *  액션을 취한 후 진동을 줄 수 있다.
+     */
+    private let softFeedback = UIImpactFeedbackGenerator(style: .soft)
+    private let rightFeedback = UIImpactFeedbackGenerator(style: .rigid)
+    
     var range: ClosedRange<Int> = 1...20
     
     var body: some View {
@@ -39,6 +46,11 @@ struct QuantitySelector: View {
     func changeQuantity(number: Int) {
         if range ~= quantity + number {
             quantity += number
+            softFeedback.prepare()
+            softFeedback.impactOccurred(intensity: 0.8)
+        } else {
+            rightFeedback.prepare()
+            rightFeedback.impactOccurred()
         }
     }
 }
